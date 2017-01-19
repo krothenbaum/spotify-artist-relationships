452c78cd7f41664e63397dcc5cf0a6f72a7f0fec
Kyle Rothenbaum <krothenbaum@gmail.com>
2017-01-18 21:01:26 -0800

    first

---
+++ app.js
@@ -0,0 +1,12 @@
+var Spotify = require('spotify-web-api-js');
+var s = new Spotify();
+// var spotifyApi = new SpotifyWebApi();
+
+
+
+// $(document).ready(function() {
+// 	spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE', function(err, data) {
+//   if (err) console.error(err);
+//   else console.log('Artist albums', data);
+// 	});
+// }
