37c1262c851c61cb6faf804885910846e172cc40
Kyle Rothenbaum <krothenbaum@gmail.com>
2017-01-26 14:30:45 -0800

    Recursion Test

--- recursion.js
+++ recursion.js
@@ -1,9 +1,8 @@
-var DEGREES = 5; //Maximum levels queried; starts at zero
+var DEGREES = 2; //Maximum levels queried; starts at zero
 
 var MAXARTISTS = 2;
 
-var ARTISTS = [];  //Multidimensional array with the number of potential 'artists' i.e. compare Madonna, to Beethoven to Eminem to nth-artist
-
+var ARTISTS = [];  //Array with the number of potential 'artists' i.e. compare Madonna, to Beethoven to Eminem to nth-artist
 
 
 $(function(){
@@ -23,7 +22,6 @@ var nextLevel = function(degree){ //Recursion
 	}
 }
 
-
 //first degree find the single artist. Following degree search recommendations for each artist in that degree's Array index
 function addNewArtist(artist, degree){
 	if (degree === 0) {
@@ -34,8 +32,6 @@ function addNewArtist(artist, degree){
 		});
 	}
 }
-
-
 //search for the artist the user inputs
 function searchArtist(artist, degree) {
 	$.ajax({
@@ -68,18 +64,31 @@ function searchRecommendations(artist, degree) {
 
 
 
-var getMatches = function(ARTISTS){  
-	var arrays = ARTISTS.slice(); //This is needed to clone the array and make a new reference 
-	var result = arrays.shift().reduce(function(res, v) {  //Fancy Pants Answer - http://stackoverflow.com/questions/11076067/finding-matches-between-multiple-javascript-arrays
-	    if (res.indexOf(v) === -1 && arrays.every(function(a) { //EXTRA CREDIT - Try to find if only some the artist arrays have matches, or which arrays match.  Say artist A with artist D match for value 'blah blah'; 
-		return a.indexOf(v) !== -1;
-	    })) 
-	    res.push(v);
-	    return res;
-	}, ['matches:']);
+// var getMatches = function(ARTISTS){  
+// 	var arrays = ARTISTS.slice(); //This is needed to clone the array and make a new reference 
+// 	var result = arrays.shift().reduce(function(res, v) {  //Fancy Pants Answer - http://stackoverflow.com/questions/11076067/finding-matches-between-multiple-javascript-arrays
+// 	    if (res.indexOf(v) === -1 && arrays.every(function(a) { //EXTRA CREDIT - Try to find if only some the artist arrays have matches, or which arrays match.  Say artist A with artist D match for value 'blah blah'; 
+// 		return a.indexOf(v) !== -1;
+// 	    })) 
+// 	    res.push(v);
+// 	    return res;
+// 	}, ['matches:']);
 
-	console.log(result)
+// 	console.log(result)
 
-}
+// }
 
-
+// function scrubArray(ARTISTS) {
+// 	alert('Scrub');
+// 	console.log(ARTISTS.length);
+// 	for(var i = 2; i < ARTISTS.length; i++) {
+// 		console.log(ARTISTS[i].artists);
+// 		for(var j = 0; j < ARTISTS[i].artists.length; j++) {
+// 			for(var k = 0; k < ARTISTS[i-1].artists.length; k++) {
+// 				if(ARTISTS[i].artists[j].id === ARTISTS[i-1].artists[k].id) {
+// 					ARTISTS[i].artists[j].splice(j, 1);
+// 				}
+// 			}
+// 		}
+// 	}
+// }
